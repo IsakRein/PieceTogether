2ce052a7c3692dc2c016f587a3bcb5a784a531c6
Isak Reinholdsson <isak.reinholdsson@gmail.com>
2018-04-05 10:47:43 +0200

    5

---
+++ Assets/Editor/GridEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(GenerateShapes))]
+public class GridEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        GenerateShapes myScript = (GenerateShapes)target;
+        if (GUILayout.Button("Generate"))
+        {
+            myScript.Generate();
+        }
+    }
+}
--- Assets/Scripts/GenerateShapes.cs
+++ Assets/Scripts/GenerateShapes.cs
@@ -1,189 +1,116 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class GenerateShapes : MonoBehaviour
-{
-    public System.Action<List<GameObject>> OnFragmentsGenerated;
+public class GenerateShapes : MonoBehaviour {
 
-    public bool allowRuntimeFragmentation = false;
-    public int extraPoints = 1;
-    public int subshatterSteps = 1;
+    public int shapeCount;
+    public int minSize;
+    public int maxSize;
 
-    public string fragmentLayer = "Default";
-    public string sortingLayerName = "Default";
-    public int orderInLayer = 0;
+    public int width;
+    public int height;
+    private int amount;
 
-    public List<GameObject> fragments = new List<GameObject>();
-    private List<List<Vector2>> polygons = new List<List<Vector2>>();
+    private int sum;
+    private int sumLeft;
 
-    public RowScript rowScript;
+    public List<int> sizes = new List<int>();
 
-    public GameObject fill;
+    public List<SpriteRenderer> squares = new List<SpriteRenderer>();
 
-    public SpriteRenderer spriteRenderer;
+    public List<int> grid = new List<int>();
 
-	public void Generate(int setExtraPoints, int setSubshatterSteps, Color32 setColor)
-	{
-        extraPoints = setExtraPoints;
-        subshatterSteps = setSubshatterSteps;
+    public List<Color32> colors = new List<Color32>();
 
-        spriteRenderer.color = setColor;
-
-        generateFragments();
-        rowScript.CustomStart();
-    }
-
-    public void Generate2()
+    private void Start()
     {
-        extraPoints = 2;
-        subshatterSteps = 0;
-
-        spriteRenderer.color = new Color32(255, 127, 127, 255);
-
-        generateFragments();
-        rowScript.CustomStart();
-    }
-
-
-    /// <summary>
-    /// Creates fragments if necessary and destroys original gameobject
-    /// </summary>
-    public void explode()
-    {
-        //if fragments were not created before runtime then create them now
-        if (fragments.Count == 0 && allowRuntimeFragmentation)
+        foreach (Transform child in transform)
         {
-            generateFragments();
+            squares.Add(child.GetComponent<SpriteRenderer>());
         }
-        //otherwise unparent and activate them
-        else
+    }
+
+    public void Generate()
+    {
+        GenerateSizes();
+
+        for (int i = 0; i < amount; i++)
         {
-            foreach (GameObject frag in fragments)
+            grid.Add(0);
+        }
+
+        for (int i = 0; i < sizes.Count; i++)
+        {
+            int firstFreeSquare = 0;
+            int num = 0;
+
+            while (firstFreeSquare == 0)
             {
-                frag.transform.parent = null;
-                frag.SetActive(true);
+                if (grid[num] == 0)
+                {
+                    firstFreeSquare = num;
+                }
+
+                num = num + 1;
             }
+
+            grid[firstFreeSquare] = i; 
         }
-        //if fragments exist destroy the original
-        if (fragments.Count > 0)
-        {
-            Destroy(gameObject);
-        }
+
+
+        ApplyColor();
     }
-    /// <summary>
-    /// Creates fragments and then disables them
-    /// </summary>
-    public void fragmentInEditor()
+
+
+    void GenerateSizes()
     {
-        if (fragments.Count > 0)
+        sizes.Clear();
+
+        amount = width * height;
+        sumLeft = amount;
+
+        for (int i = 0; i < shapeCount; i++)
         {
-            deleteFragments();
-        }
-        generateFragments();
-        setPolygonsForDrawing();
-        foreach (GameObject frag in fragments)
-        {
-            frag.transform.parent = transform;
-            frag.SetActive(false);
-        }
-    }
-    public void deleteFragments()
-    {
-        foreach (GameObject frag in fragments)
-        {
-            if (Application.isEditor)
+            if ((minSize * (shapeCount - sizes.Count) <= sumLeft) && (maxSize * (shapeCount - sizes.Count) >= sumLeft))
             {
-                DestroyImmediate(frag);
+                if (i == shapeCount - 1)
+                {
+                    sizes.Add(sumLeft);
+                }
+                else
+                {
+                    sizes.Add(Random.Range(minSize, maxSize + 1));
+                }
+                CalculateSum();
             }
+
             else
             {
-                Destroy(frag);
-            }
-        }
-        fragments.Clear();
-        polygons.Clear();
-    }
-    /// <summary>
-    /// Turns Gameobject into multiple fragments
-    /// </summary>
-    private void generateFragments()
-    {
-        fragments = new List<GameObject>();
-        fragments = SpriteExploder.GenerateVoronoiPieces(gameObject, extraPoints, subshatterSteps);
-
-        //sets additional aspects of the fragments
-        foreach (GameObject p in fragments)
-        {
-            if (p != null)
-            {
-                p.layer = LayerMask.NameToLayer(fragmentLayer);
-                p.GetComponent<Renderer>().sortingLayerName = sortingLayerName;
-                p.GetComponent<Renderer>().sortingOrder = orderInLayer;
+                sizes.RemoveAt(i - 1);
+                CalculateSum();
+                i = i - 2;
             }
         }
 
-        foreach (ExplodableAddon addon in GetComponents<ExplodableAddon>())
+        CalculateSum();
+    }
+
+    void CalculateSum()
+    {
+        sum = 0;
+        for (int j = 0; j < sizes.Count; j++)
         {
-            if (addon.enabled)
-            {
-                addon.OnFragmentsGenerated(fragments);
-            }
+            sum = sum + sizes[j];
         }
+        sumLeft = amount - sum;
     }
-    private void setPolygonsForDrawing()
-    {
-        polygons.Clear();
-        List<Vector2> polygon;
 
-        foreach (GameObject frag in fragments)
+    void ApplyColor()
+    {
+        for (int i = 0; i < amount; i++)
         {
-            polygon = new List<Vector2>();
-            foreach (Vector2 point in frag.GetComponent<PolygonCollider2D>().points)
-            {
-                Vector2 offset = rotateAroundPivot((Vector2)frag.transform.position, (Vector2)transform.position, Quaternion.Inverse(transform.rotation)) - (Vector2)transform.position;
-                offset.x /= transform.localScale.x;
-                offset.y /= transform.localScale.y;
-                polygon.Add(point + offset);
-            }
-            polygons.Add(polygon);
-        }
-    }
-    private Vector2 rotateAroundPivot(Vector2 point, Vector2 pivot, Quaternion angle)
-    {
-        Vector2 dir = point - pivot;
-        dir = angle * dir;
-        point = dir + pivot;
-        return point;
-    }
-
-    void OnDrawGizmos()
-    {
-        if (Application.isEditor)
-        {
-            if (polygons.Count == 0 && fragments.Count != 0)
-            {
-                setPolygonsForDrawing();
-            }
-
-            Gizmos.color = Color.blue;
-            Gizmos.matrix = transform.localToWorldMatrix;
-            Vector2 offset = (Vector2)transform.position * 0;
-            foreach (List<Vector2> polygon in polygons)
-            {
-                for (int i = 0; i < polygon.Count; i++)
-                {
-                    if (i + 1 == polygon.Count)
-                    {
-                        Gizmos.DrawLine(polygon[i] + offset, polygon[0] + offset);
-                    }
-                    else
-                    {
-                        Gizmos.DrawLine(polygon[i] + offset, polygon[i + 1] + offset);
-                    }
-                }
-            }
+            squares[i].color = colors[grid[i]];
         }
     }
 }
