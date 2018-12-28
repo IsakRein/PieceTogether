fa4bce373d67091920a7c8c1a98118ea9d4df955
Isak Reinholdsson <isak.reinholdsson@gmail.com>
2018-12-28 15:09:21 +0100

    58

--- Assets/Scripts/5. Settings/PopUp2.cs
+++ Assets/Scripts/5. Settings/PopUp2.cs
@@ -64,7 +64,7 @@ public class PopUp2 : MonoBehaviour
 
     public void DeSelectProduct()
     {
-        foreach (Transform child in transform)
+        foreach (Transform child in transform.GetChild(0))
         {
             child.GetComponent<Animator>().SetTrigger("Stop");
         }
